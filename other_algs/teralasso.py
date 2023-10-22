44c3a0c40b61432a628f79a5ce94eea0b172ef18
AIStats-GmGM <147455573+AIStats-GmGM@users.noreply.github.com>
2023-10-22 18:34:10 +0100

    Add files via upload

---
+++ other_algs/teralasso.py
@@ -0,0 +1,74 @@
+"""
+This is a python wrapper for TeraLasso
+"""
+
+import matlab.engine
+import io
+import numpy as np
+from GmGM import nmode_gram
+
+eng = matlab.engine.start_matlab()
+eng.addpath(
+    '../other_algs/teralasso'
+)
+
+
+def TeraLasso(Ys, betas, max_iter=100):
+    n, *d = Ys.shape
+    
+    K = len(Ys.shape[1:])
+    Ss = [
+        matlab.double(
+            nmode_gram(Ys, ell+1)
+        )
+        for ell in range(K)
+    ]
+    
+    d_matlab = matlab.double(d)
+    betas_matlab = matlab.double(betas)
+
+    Psis_matlab = eng.teralasso(
+        Ss,
+        d_matlab,
+        'L1',
+        0,
+        1e-8,
+        betas_matlab,
+        max_iter,
+        nargout=1,
+        stdout=io.StringIO()
+    )
+    
+    Psis = []
+    
+    for Psi in Psis_matlab:
+        Psis.append(np.asarray(Psi))
+    
+    return Psis
+    
+def TeraLasso_cov(Ss, betas, max_iter=100):
+    
+    d = [S.shape[0] for S in Ss]
+    Ss = [matlab.double(S) for S in Ss]
+    
+    d_matlab = matlab.double(d)
+    betas_matlab = matlab.double(betas)
+
+    Psis_matlab = eng.teralasso(
+        Ss,
+        d_matlab,
+        'L1',
+        0,
+        1e-8,
+        betas_matlab,
+        max_iter,
+        nargout=1,
+        stdout=io.StringIO()
+    )
+    
+    Psis = []
+    
+    for Psi in Psis_matlab:
+        Psis.append(np.asarray(Psi))
+    
+    return Psis
